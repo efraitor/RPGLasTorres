a60b2bc553a3a3d1fc0c17c20fe7b7e75d736ca9
efraitor_ <efraitor.contact@gmail.com>
2023-03-23 20:04:06 +0100

    Changes
    -Look good in directions

--- Assets/Scripts/LevelMechanics/AreaEntrance.cs
+++ Assets/Scripts/LevelMechanics/AreaEntrance.cs
@@ -7,6 +7,9 @@ public class AreaEntrance : MonoBehaviour
     //El nombre del área de la escena donde queremos aparecer
     public string transitionName;
 
+    //Dos variables para conocer la dirección en la que debe mirar el jugador
+    public float dirX, dirY;
+
     // Start is called before the first frame update
     void Start()
     {   
@@ -15,6 +18,11 @@ public class AreaEntrance : MonoBehaviour
         {
             //Igualamos la posición del jugador a la de esa área
             PlayerController.instance.transform.position = transform.position;
+            //Accedemos al animator del jugador y le ponemos la dirección en la que tiene que mirar inicialmente
+            PlayerController.instance.anim.SetFloat("lastMoveX", dirX);
+            PlayerController.instance.anim.SetFloat("lastMoveY", dirY);
+            //Iniciamos el tiempo de no Input del jugador
+            PlayerController.instance.InitialiceNoInput();
         }
     }
 
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,7 +13,11 @@ public class PlayerController : MonoBehaviour
     //Referencia al RigidBody del jugador
     private Rigidbody2D theRB;
     //Referencia al Animator del jugador
-    private Animator anim;
+    public Animator anim;
+
+    //Temporizador sin Input
+    public float noMoveLength;
+    private float noMoveCount;
 
     //Hacemos una referencia (Singleton) 
     public static PlayerController instance;
@@ -39,27 +43,38 @@ public class PlayerController : MonoBehaviour
         theRB = GetComponent<Rigidbody2D>();
         //Inicializamos el Animator del jugador
         anim = GetComponent<Animator>();
-    
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Movemos al personaje usando la velocidad de su RigidBody, obteniendo los Inputs de los ejes de movimiento
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        if(noMoveCount <= 0)
+        {
+            //Movemos al personaje usando la velocidad de su RigidBody, obteniendo los Inputs de los ejes de movimiento
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+            
 
+            //Si hemos pulsado cualquiera de los botones de dirección
+            if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+            {
+                //Metemos como última posición en X e Y el último input realizado
+                anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+        }
+        else
+            noMoveCount -= Time.deltaTime;
 
         //ANIMACIONES
         anim.SetFloat("moveX", theRB.velocity.x);
         anim.SetFloat("moveY", theRB.velocity.y);
-
-        //Si hemos pulsado cualquiera de los botones de dirección
-        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
-        {
-            //Metemos como última posición en X e Y el último input realizado
-            anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
-        }
     }
 
+    //Método para inicializar el contador
+    public void InitialiceNoInput()
+    {
+        //Inicializamos el contador de no Input
+        noMoveCount = noMoveLength;
+        theRB.velocity = Vector2.zero;
+    }
 }
